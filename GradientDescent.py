98a552e9a2fab6be7f80c1918463e55ba9cbd77e
Seungmin Jeong <sontneptm2@gmail.com>
2022-06-05 21:16:57 +0900

    update

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,15 +1,24 @@
-from numpy import gradient
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.losses import MSE
+
+"""
+this is psuedocode
+"""
+
+x = None
+y = None
 
 model = Sequential()
-
+optimizer = SGD()
 weights = tf.Variable([tf.random.normal()])
 
 while True: # loop forever
-    with tf.GradientTape() as g:
-        loss = compute_loss(weights)
-        gradient = g.gradient(loss, weights)
+    prediction = model(x)
 
-    weights = weights - lr * gradient
+    with tf.GradientTape() as tape:
+        loss = MSE(y, prediction)
+        
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
