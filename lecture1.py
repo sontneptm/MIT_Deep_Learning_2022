4de7dd4b2a1ab5e4cebc88a53e6b4f565bb10caf
Seungmin Jeong <sontneptm2@gmail.com>
2022-06-05 17:30:34 +0900

    update

--- lecture1.py
+++ lecture1.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
 
-class 
+class MyDenseLayer(tf.keras.layers.layer):
+    def __init__(self, input_dim, output_dim) -> None:
+        super(MyDenseLayer, self).__init__()
+
+        self.weights = self.add_weight([input_dim, output_dim])
+        self.bias = self.add_weight([1, output_dim])
+
+    def call(self, inputs):
+        # Forward propagate the inputs
+        z = tf.matmul(inputs, self.weights) + self.bias
+
+        output = tf.math.sigmoid(z)
+
+        return output
